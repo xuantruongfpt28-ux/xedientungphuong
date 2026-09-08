2d877436a7a0491ab43f6fc02e94de71501a4aa2
xuantruongfpt28-ux <xuantruongfpt28@gmail.com>
2026-09-08 13:30:02 +0700

    Update source code

--- new-main/quanlicuahang-main/cua-hang-frontend/src/supabase.ts
+++ new-main/quanlicuahang-main/cua-hang-frontend/src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://eaxeqenkavftvktewvyk.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_4OIIxAffuM6phgLFkAIJXQ_EcHn4EMu';
+const SUPABASE_URL = 'https://qizumogeilrainjbstcq.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable__ukhoS90rN40bd7IwbY4LA_2Q6oD5Zy';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
